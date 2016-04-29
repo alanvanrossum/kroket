27c16e4af6b2619716fad47ca451747f4dd65aee
harveyvanveltom <harvey-shadow@hotmail.com>
2016-04-29 13:54:33 +0200

    removed dependencies for testing purposes

--- Escape_Room/src/main/java/com/mycompany/escape_room/Main.java
+++ Escape_Room/src/main/java/com/mycompany/escape_room/Main.java
@@ -11,34 +11,7 @@ VR Instancing Progress:
 
 package com.mycompany.escape_room;
 
-import com.jme3.input.InputManager;
-import com.jme3.input.KeyInput;
-import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.KeyTrigger;
-import com.jme3.material.Material;
-import com.jme3.math.FastMath;
-import com.jme3.math.Quaternion;
-import com.jme3.math.Vector2f;
-import com.jme3.math.Vector3f;
-import com.jme3.post.FilterPostProcessor;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.Node;
-import com.jme3.scene.Spatial;
-import com.jme3.scene.Spatial.CullHint;
-import com.jme3.scene.shape.Box;
-import com.jme3.texture.Texture;
-import com.jme3.texture.Texture.MagFilter;
-import com.jme3.texture.Texture.MinFilter;
-import com.jme3.ui.Picture;
-import com.jme3.util.SkyFactory;
-import jmevr.app.VRApplication;
-import jmevr.input.OpenVR;
-import jmevr.input.VRBounds;
-import jmevr.input.VRInput;
-import jmevr.input.VRInput.VRINPUT_TYPE;
-import jmevr.post.CartoonSSAO;
-import jmevr.util.VRGuiManager;
-import jmevr.util.VRGuiManager.POSITIONING_MODE;
+
 
 /**
  *
