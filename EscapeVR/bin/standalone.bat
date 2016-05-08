9bd694dcb6e5a9e9ea6c6e44bf678094fe002854
Jochem <J.W.Heijltjes@student.tudelft.nl>
2016-05-09 00:53:41 +0200

    Fix project setup.

---
+++ EscapeVR/bin/standalone.bat
@@ -0,0 +1,4 @@
+@echo off
+set basedir=%CD%
+"java.exe" -Dfile.encoding=UTF-8 -classpath %basedir%\jme_vr_libs\JMonkeyVR.jar;%basedir%\jme_vr_libs\j-ogg-all.jar;%basedir%\jme_vr_libs\jme3-blender.jar;%basedir%\jme_vr_libs\jme3-core.jar;%basedir%\jme_vr_libs\jme3-desktop.jar;%basedir%\jme_vr_libs\jme3-effects.jar;%basedir%\jme_vr_libs\jme3-jogg.jar;%basedir%\jme_vr_libs\jme3-lwjgl3.jar;%basedir%\jme_vr_libs\jme3-plugins.jar;%basedir%\jme_vr_libs\jna-4.2.1.jar;%basedir%\jme_vr_libs\lwjgl-3.0.0-natives.jar;%basedir%\jme_vr_libs\lwjgl-3.0.0.jar;%basedir%\build\classes;%basedir%\assets nl.tudelft.kroket.escape.Main
+pause
