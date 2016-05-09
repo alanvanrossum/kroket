ffafc1e56473406f7075bf33092ea1e1a42a4e60
Irene van der Blij <i.c.vanderblij@student.tudelft.nl>
2016-05-09 15:14:50 +0200

    Working code image

--- EscapeApp/EscapeApp/app/src/main/java/com/context/kroket/escapeapp/Game_A_Activity.java
+++ EscapeApp/EscapeApp/app/src/main/java/com/context/kroket/escapeapp/Game_A_Activity.java
@@ -13,8 +13,8 @@ public class Game_A_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_a);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
