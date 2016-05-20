35271882baa28621aee8346d2e28aec818725e30
harveyvanveltom <harvey-shadow@hotmail.com>
2016-05-20 23:05:33 +0200

    removed checkstyle, pmd and findbugs errors for the EscapeServer.

--- EscapeServer/src/main/java/nl/tudelft/kroket/server/Settings.java
+++ EscapeServer/src/main/java/nl/tudelft/kroket/server/Settings.java
@@ -8,18 +8,18 @@ package nl.tudelft.kroket.server;
  *
  */
 public class Settings {
-	/** Port number to bind server to. */
-	public static final int PORTNUM = 1234;
+  /** Port number to bind server to. */
+  public static final int PORTNUM = 1234;
 
-	/** Number of VR clients required to start the game. */
-	public static final int REQUIRED_VIRTUAL = 1;
+  /** Number of VR clients required to start the game. */
+  public static final int REQUIRED_VIRTUAL = 1;
 
-	/** Number of mobile clients required to start the game. */
-	public static final int REQUIRED_MOBILE = 1;
+  /** Number of mobile clients required to start the game. */
+  public static final int REQUIRED_MOBILE = 1;
 
-	/** Number of seconds before each status print. */
-	public static final int INTERVAL_REPORT_STATUS = 30;
+  /** Number of seconds before each status print. */
+  public static final int INTERVAL_REPORT_STATUS = 30;
 
-	/** Number of seconds until retrying to bind to the socket. */
-	public static final int INTERVAL_SOCKET_RETRY = 5;
+  /** Number of seconds until retrying to bind to the socket. */
+  public static final int INTERVAL_SOCKET_RETRY = 5;
 }
