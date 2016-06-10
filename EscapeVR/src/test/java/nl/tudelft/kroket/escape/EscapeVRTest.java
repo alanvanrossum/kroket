bd1ac59b654689fd5a1ad06908ef75141ec0b841
Mayke93 <m.l.kloppenburg@student.tudelft.nl>
2016-06-10 16:11:27 +0200

    add test, correct loggertest

--- EscapeVR/src/test/java/nl/tudelft/kroket/escape/EscapeVRTest.java
+++ EscapeVR/src/test/java/nl/tudelft/kroket/escape/EscapeVRTest.java
@@ -1,60 +1,44 @@
-/**
- * 
- */
 package nl.tudelft.kroket.escape;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * Test for EscapeVR class.
+ * @author Team Kroket
+ *
+ */
 public class EscapeVRTest {
 
-//  /**
-//   * Test method for {@link nl.tudelft.kroket.escape.EscapeVR#simpleUpdate(float)}.
-//   */
-//  @Test
-//  public void testSimpleUpdate() {
-//    fail("Not yet implemented");
-//  }
-//
-//  /**
-//   * Test method for {@link nl.tudelft.kroket.escape.EscapeVR#simpleInitApp()}.
-//   */
-//  @Test
-//  public void testSimpleInitApp() {
-//    fail("Not yet implemented");
-//  }
-//
-//  /**
-//   * Test method for {@link nl.tudelft.kroket.escape.EscapeVR#createLabel(com.jme3.asset.AssetManager, java.lang.String, float, float, float, float)}.
-//   */
-//  @Test
-//  public void testCreateLabel() {
-//    fail("Not yet implemented");
-//  }
-//
-//  /**
-//   * Test method for {@link nl.tudelft.kroket.escape.EscapeVR#setGameState(nl.tudelft.kroket.escape.EscapeVR.GameState)}.
-//   */
-//  @Test
-//  public void testSetGameState() {
-//    fail("Not yet implemented");
-//  }
-//
-//  /**
-//   * Test method for {@link nl.tudelft.kroket.escape.EscapeVR#remoteInput(java.lang.String)}.
-//   */
-//  @Test
-//  public void testRemoteInput() {
-//    fail("Not yet implemented");
-//  }
-//
-//  /**
-//   * Test method for {@link nl.tudelft.kroket.escape.EscapeVR#receiveLoop(java.lang.String)}.
-//   */
-//  @Test
-//  public void testReceiveLoop() {
-//    fail("Not yet implemented");
-//  }
-
+  /**
+   * Test method for getter and setter of remotehost.
+   */
+  @Test
+  public void testGetSetRemoteHost() {
+    EscapeVR vr = new EscapeVR();
+    vr.setRemoteHost("test");
+    assertEquals(vr.getRemoteHost(), "test");  
+  }
+  
+  /**
+   * Test method for getter and setter of remote port.
+   */
+  @Test
+  public void testGetSetRemoteport() {
+    EscapeVR vr = new EscapeVR();
+    vr.setRemotePort(4);
+    assertEquals(vr.getRemotePort(), 4);  
+  }
+  
+  /**
+   * Test method for getter and setter for the playername.
+   */
+  @Test
+  public void testGetSetPlayername() {
+    EscapeVR vr = new EscapeVR();
+    vr.setPlayerName("test");
+    assertEquals(vr.getPlayerName(), "test");  
+  }
+  
 }
