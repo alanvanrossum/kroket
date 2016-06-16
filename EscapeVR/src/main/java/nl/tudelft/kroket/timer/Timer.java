00fdcc0846da2c58146cb801043783b290506722
Mayke93 <m.l.kloppenburg@student.tudelft.nl>
2016-06-16 10:48:33 +0200

    fix formatting

--- EscapeVR/src/main/java/nl/tudelft/kroket/timer/Timer.java
+++ EscapeVR/src/main/java/nl/tudelft/kroket/timer/Timer.java
@@ -13,89 +13,89 @@ import nl.tudelft.kroket.screen.HeadUpDisplay;
  */
 public class Timer {
 
-	/** Singleton logger instance. */
-	static final Logger log = Logger.getInstance();
+  /** Singleton logger instance. */
+  static final Logger log = Logger.getInstance();
 
-	/** Current class, used as tag for logger. */
-	private String className = this.getClass().getSimpleName();
+  /** Current class, used as tag for logger. */
+  private String className = this.getClass().getSimpleName();
 
-	private long timeLimit;
-	private boolean active;
-	
-	private ClientThread clientThread;
-	private HeadUpDisplay hud;
+  private long timeLimit;
+  private boolean active;
 
-	/**
-	 * Constructor for the timer object.
-	 * 
-	 * @param clientThread the client thread for sending messages to the server.
-	 * @param hud the head up display.
-	 */
-	public Timer(ClientThread clientThread, HeadUpDisplay hud) {
-		active = false;
-		this.clientThread = clientThread;
-		this.hud = hud;
-	}
+  private ClientThread clientThread;
+  private HeadUpDisplay hud;
 
-	/**
-	 * Starts the timer.
-	 */
-	public void startTimer() {
-		log.info(className, "Starting timer...");
-		timeLimit = Settings.TIMELIMIT * 1000 + System.currentTimeMillis();
-		active = true;
-	}
+  /**
+   * Constructor for the timer object.
+   * 
+   * @param clientThread the client thread for sending messages to the server.
+   * @param hud the head up display.
+   */
+  public Timer(ClientThread clientThread, HeadUpDisplay hud) {
+    active = false;
+    this.clientThread = clientThread;
+    this.hud = hud;
+  }
 
-	/**
-	 * Stops the timer.
-	 */
-	public void stopTimer() {
-		log.info(className, "Stopping timer...");				
-		clientThread.sendMessage(String.format("%s", Protocol.COMMAND_GAMEOVER));
-		active = false;
-	}
+  /**
+   * Starts the timer.
+   */
+  public void startTimer() {
+    log.info(className, "Starting timer...");
+    timeLimit = Settings.TIMELIMIT * 1000 + System.currentTimeMillis();
+    active = true;
+  }
 
-	/**
-	 * Checks whether the timer is active.
-	 * 
-	 * @return true iff the timer is active.
-	 */
-	public boolean isActive() {
-		return active;
-	}
+  /**
+   * Stops the timer.
+   */
+  public void stopTimer() {
+    log.info(className, "Stopping timer...");
+    clientThread.sendMessage(String.format("%s", Protocol.COMMAND_GAMEOVER));
+    active = false;
+  }
 
-	/**
-	 * Updates the timer.
-	 */
-	public void update() {
-		if (isActive()) {
-			
-			long timeRemaining = Math.max(timeLimit - System.currentTimeMillis(), 0);
-			
-			if (timeRemaining <= 0) {
-				stopTimer();
-				return;
-			}
+  /**
+   * Checks whether the timer is active.
+   * 
+   * @return true iff the timer is active.
+   */
+  public boolean isActive() {
+    return active;
+  }
 
-		    int secondsRemaining = (int) (timeRemaining / 1000);
+  /**
+   * Updates the timer.
+   */
+  public void update() {
+    if (isActive()) {
 
-		    int minutesRemaining = secondsRemaining / 60;
-		    secondsRemaining -= (minutesRemaining * 60);
+      long timeRemaining = Math.max(timeLimit - System.currentTimeMillis(), 0);
 
-		    hud.setTimerText(String.format("Time remaining: %02d:%02d", minutesRemaining,
-		            secondsRemaining));
-		}
-	}
+      if (timeRemaining <= 0) {
+        stopTimer();
+        return;
+      }
 
-	/**
-	 * Extend the time left and send this to the virtual client.
-	 * 
-	 * @param time
-	 *          the time to be added in seconds.
-	 */
-	public void bonusTime() {
-		log.info(className, String.format("Timer increased by %d seconds.", Settings.BONUSTIME));
-		timeLimit += Settings.BONUSTIME * 1000;
-	}
+      int secondsRemaining = (int) (timeRemaining / 1000);
+
+      int minutesRemaining = secondsRemaining / 60;
+      secondsRemaining -= (minutesRemaining * 60);
+
+      hud.setTimerText(String.format("Time remaining: %02d:%02d", minutesRemaining,
+          secondsRemaining));
+    }
+  }
+
+  /**
+   * Extend the time left and send this to the virtual client.
+   * 
+   * @param time
+   *          the time to be added in seconds.
+   */
+  public void bonusTime() {
+    log.info(className, String.format("Timer increased by %d seconds.", Settings.BONUSTIME));
+    timeLimit += Settings.BONUSTIME * 1000;
+  }
 
 }
